a66b4bdf80f568f216f69bd99ec9fa432e5c1f1f
darpacfo <lucasmrasmussen2@gmail.com>
2024-10-18 12:39:52 +0200

    element formatting abstract made fund class made
    Basic getters and setters made for these

---
+++ src/elementFormatting.java
@@ -0,0 +1,51 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class elementFormatting {
+    private String title;
+    private ArrayList<String> categories = new ArrayList<String>();
+    private String description;
+    private LocalDateTime dateCreated = LocalDateTime.now();
+
+    public String getTitle(){
+        return title;
+    }
+
+    public ArrayList<String> getCategories(){
+        return categories;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public LocalDateTime getDateCreated(){
+        return dateCreated;
+    }
+
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+
+    public void setCategories(String newCategory){
+        this.categories.add(newCategory);
+    }
+
+    public void setDescription(String newDescription){
+        this.description = newDescription;
+    }
+
+    public static void main(String[] args) {
+        String[] testCatories = {"cat1","cat2","cat3"};
+        elementFormatting abstractObject = new elementFormatting();
+        abstractObject.setTitle("testTitle");
+        abstractObject.setDescription("Lorem Ipsum");
+        for (int i = 0; i < testCatories.length ; i++){
+            abstractObject.setCategories(testCatories[i]);
+        }
+        System.out.println("Here is the abstractElement: " + abstractObject.getTitle());
+        System.out.println("It has the following description: " + abstractObject.getDescription());
+        System.out.println("It was created at: " + abstractObject.getDateCreated());
+        System.out.println("It has the following categoreis: " + abstractObject.getCategories());
+    }
+}
---
+++ src/fundClass.java
@@ -0,0 +1,74 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class fundClass extends elementFormatting{
+    private ArrayList<String> deadlines = new ArrayList<String>();
+    private ArrayList<String> contacts = new ArrayList<String>();
+    private long commonBudget = 0; //Handles large numbers
+    private ArrayList<String> collaborationHistory= new ArrayList<String>();
+
+    public ArrayList<String> getDeadlines(){
+        return deadlines;
+    }
+
+    public ArrayList<String> getContacts(){
+        return contacts;
+    }
+
+    public long getCommonBudget(){
+        return commonBudget;
+    }
+
+    public ArrayList<String> getCollaborationHistory(){
+        return collaborationHistory;
+    }
+
+    public void setDeadlines(String newDeadline){
+        this.deadlines.add(newDeadline);
+    }
+
+    public void setContacts(String newContact){
+        this.contacts.add(newContact);
+    }
+
+    public void setCommonBudget(long newBudget){
+        this.commonBudget = newBudget;
+    }
+
+    public void setCollaborationHistory(String newCollaboration){
+        this.collaborationHistory.add(newCollaboration);
+    }
+
+    public static void main(String[] args) {
+        String[] testCatories = {"cat1","cat2","cat3"};
+        String[] testDeadlines = {"2.10.2024","4.12.2024"};
+        String[] testContacts = {"lucas","lundse"};
+        String[] testCollaborationHistory = {"P1","P2"};
+
+        fundClass fund = new fundClass();
+        fund.setTitle("testTitle2");
+        fund.setDescription("Lorem Ipsum");
+        fund.setCommonBudget(80000000);
+        for (int i = 0; i < testCatories.length ; i++){
+            fund.setCategories(testCatories[i]);
+        }
+        for (int i = 0; i < testDeadlines.length ; i++){
+            fund.setDeadlines(testDeadlines[i]);
+        }
+        for (int i = 0; i < testContacts.length ; i++){
+            fund.setContacts(testContacts[i]);
+        }
+        for (int i = 0; i < testCollaborationHistory.length ; i++){
+            fund.setCollaborationHistory(testCollaborationHistory[i]);
+        }
+        System.out.println("Here is the fund: " + fund.getTitle());
+        System.out.println("It has the following description: " + fund.getDescription());
+        System.out.println("It was created at: " + fund.getDateCreated());
+        System.out.println("It has the following categoreis: " + fund.getCategories());
+        System.out.println("The deadlines for the fund is: " + fund.getDeadlines());
+        System.out.println("Contact persons at the fund are: " + fund.getContacts());
+        System.out.println("The fund has a budget of: " + fund.getCommonBudget());
+        System.out.println("We have previously collaborated on: " + fund.getCollaborationHistory());
+    }
+
+}
