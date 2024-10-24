7386eeb22f55b8c215e281c89394579af6403d60
lund <elund23@student.aau.dk>
2024-10-24 11:05:02 +0200

    x

---
+++ src/ListHandling/listImporting.java
@@ -0,0 +1,11 @@
+// package ListHandling;
+// import java.util.ArrayList;
+
+// public class listImporting {
+//     // Import all the projects and store in Arraylist
+//     public static ArrayList<String> importProjects(project){ {
+//         ArrayList<String> projects = new ArrayList<>();
+//         projects.add(project);
+//         return projects;
+//     }
+// }
