57004fe842b935b0adbe7376a026cd9f1bc2f1c4
lund <elund23@student.aau.dk>
2024-10-24 11:05:02 +0200

    x

---
+++ src/projectConstructor.java
@@ -0,0 +1,10 @@
+// public class projectConstructor extends projectAbstract {
+
+// //Import project proposal and create a new project
+// public proposalToProjectConstructor() {
+    
+    
+// }
+// public projectConstructor() {
+   
+// }
