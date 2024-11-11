eddcf17fd60d75b1de1d3fe85b46c645dbe07d84
Fenrir <fenrir.lupum@gmail.com>
2024-11-11 10:35:08 +0100

    cleaned up src moved projectConstructor to a factory renamed functions in projectAbstract to be more descriptive

--- src/factory.java
+++ src/factory.java
@@ -1,19 +1,23 @@
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-public class projectConstructor extends projectAbstract {
-//Import project proposal and create a new project
-    public project proposalToProjectConstructor(proposalProject proposal, boolean onlyOneIsNeeded, ArrayList<fundClass> fundList){
+
+public class factory extends projectAbstract {
+    // Import project proposal and create a new project
+    public project createProjectFromProposal(proposalProject proposal, boolean onlyOneIsNeeded,
+            ArrayList<fundClass> fundList) {
         sortingFundLists sorter = new sortingFundLists();
         project newProject = new project();
-        for(int i = 0; i < fundList.size(); i++){
+        for (int i = 0; i < fundList.size(); i++) {
             System.out.println("------------");
             System.out.println(fundList.get(i).getTitle());
             System.out.println("------------");
         }
         newProject.setDeadlines(sorter.sortFunds(fundList, newProject, onlyOneIsNeeded));
         newProject.setTitle(proposal.getTitle());
-        newProject.setProjectCatagories(proposal.getCategories());
+
+        for (String s : proposal.getCategories()) {
+            newProject.setCategories(s);
+        }
         newProject.setDescription(proposal.getDescription());
         newProject.setProjectPurpose(proposal.getProjectPurpose());
         newProject.setProjectOwner(proposal.getProjectOwner());
