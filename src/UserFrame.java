733677926ee4ebca67652d07d212d43a02ddcfff
waltherwiwe <waltherwiwe@gmail.com>
2024-11-13 09:08:37 +0100

    Still needed to have the fund list connected to main

--- src/UserFrame.java
+++ src/UserFrame.java
@@ -346,9 +346,12 @@ public class UserFrame implements ActionListener {
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
             // Add more fields as needed
 >>>>>>> fc8f243 (Created archive button functionality with funds,)
+=======
+>>>>>>> 8541e60 (Still needed to have the fund list connected to)
         } else if (item instanceof proposalProject) {
             proposalProject proposal = (proposalProject) item;
             detailsPanel.add(new JLabel("Titel: " + proposal.getTitle()));
@@ -370,9 +373,12 @@ public class UserFrame implements ActionListener {
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
             // Add more fields as needed
 >>>>>>> fc8f243 (Created archive button functionality with funds,)
+=======
+>>>>>>> 8541e60 (Still needed to have the fund list connected to)
         } else if (item instanceof fundClass) {
             fundClass fund = (fundClass) item;
             detailsPanel.add(new JLabel("Titel: " + fund.getTitle()));
@@ -1336,9 +1342,12 @@ private void showFundDetails(fundClass fund) {
     
     archiveButton.addActionListener(e -> {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         // Archive the project
 >>>>>>> fc8f243 (Created archive button functionality with funds,)
+=======
+>>>>>>> 8541e60 (Still needed to have the fund list connected to)
         archive.archiveFund(fund);
 
         // Call update methods after archiving
